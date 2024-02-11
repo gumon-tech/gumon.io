59a164427c5e422378a371f49c41ce81011dff72
komphet <komphet.me@gmail.com>
2024-02-11 07:54:12 +0700

    Change from static html page to nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+import Image from "next/image";
+import random from "random";
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+const dayjs = require("dayjs");
+import { useState, useEffect } from "react";
+
+function getWindowDimensions() {
+  if (typeof window !== "undefined") {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  } else {
+    return {
+      width: 0,
+      height: 0,
+    };
+  }
+}
+
+export default function Home() {
+  const luckyBgNumber = random.int(1, 16);
+  const luckyLogoNumber = random.int(1, 20);
+  const [isClient, setIsClient] = useState(false);
+  const [isComplete, setComplete] = useState(false);
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  const [digitBlockStyle, setDigitBlockStyle] = useState({
+    width: 46,
+    height: 80,
+    fontSize: 50,
+  });
+
+  function handleResize() {
+    const currentWindowDimensions = getWindowDimensions();
+    setWindowDimensions(currentWindowDimensions);
+
+    if (currentWindowDimensions.width < 450) {
+      setDigitBlockStyle({ width: 30, height: 50, fontSize: 20 });
+    } else if (currentWindowDimensions.width < 500) {
+      setDigitBlockStyle({ width: 40, height: 60, fontSize: 30 });
+    } else {
+      setDigitBlockStyle({ width: 46, height: 80, fontSize: 50 });
+    }
+  }
+
+  useEffect(() => {
+    setIsClient(true);
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const play = () => {
+    if (isClient) {
+      var audio: any = document.getElementById("a1");
+      audio.play();
+    }
+  };
+
+  return (
+    <main>
+      {isClient && (
+        <video autoPlay loop muted className="bg-video">
+          <source src={`bg/${luckyBgNumber}.mp4`} />
+        </video>
+      )}
+
+      <div className="content flex justify-center items-center flex-col text-indigo-900">
+        {isClient && (
+          <Image
+            src={`/logo/${luckyLogoNumber}.png`}
+            width={200}
+            height={200}
+            alt="Picture of the author"
+          />
+        )}
+
+        <h2 className="m-5 text-center inline-flex items-center rounded-md bg-blue-50 px-7 py-5 text-3xl font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+          {isComplete ? "WELCOME TO GUMON!" : "GUMON IS COMING SOON!"}
+        </h2>
+        {isClient && !isComplete && (
+          <FlipClockCountdown
+            onTick={play}
+            to={dayjs("2024-03-01 15:00:00.000+07:00")}
+            onComplete={() => setComplete(true)}
+            digitBlockStyle={digitBlockStyle}
+          />
+        )}
+        <a href="mailto:contact@gumon.io">
+          <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+            contact@gumon.io
+          </span>
+        </a>
+        <audio autoPlay={true} id="a1">
+          <source src="/sound/back-tick-107822.mp3" type="audio/mpeg" />
+        </audio>
+      </div>
+    </main>
+  );
+}
